0c842039238da605360f54ea273e6fd501a7421c
Kiran Shila <me@kiranshila.com>
2020-08-09 15:21:56 -0400

    Copied the node_script target to try to build a custom target

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,4 @@
   (cider-custom-cljs-repl-init-form . "(do (require 'esprit.repl)
                                            (cider.piggieback/cljs-repl (esprit.repl/repl-env :endpoint-address \"192.168.4.69\")))")
   (cider-default-cljs-repl . custom)
-  (cider-print-fn . pr)
-  (eval . (message "Hello")))
+  (cider-print-fn . pr)))
---
+++ src/hackerhud/target.clj
@@ -0,0 +1,86 @@
+(ns hackerhud.target
+  (:refer-clojure :exclude (flush))
+  (:require [clojure.set :as set]
+            [clojure.string :as str]
+            [cljs.compiler :as cljs-comp]
+            [clojure.spec.alpha :as s]
+            [shadow.cljs.repl :as repl]
+            [shadow.build.node :as node]
+            [shadow.build :as comp]
+            [shadow.build.targets.shared :as shared]
+            [shadow.build.config :as config]
+            [shadow.build.api :as cljs]
+            ))
+
+(s/def ::main shared/unquoted-qualified-symbol?)
+
+(s/def ::target
+  (s/keys
+    :req-un
+    [::main
+     ::shared/output-to]
+    :opt-un
+    [::shared/output-dir]
+    ))
+
+(defmethod config/target-spec :node-script [_]
+  (s/spec ::target))
+
+(defmethod config/target-spec `process [_]
+  (s/spec ::target))
+
+(defn configure [state mode config]
+  (-> state
+      (shared/set-output-dir mode config)
+
+      (node/set-defaults)
+      (node/configure config)
+
+      (assoc-in [:compiler-options :closure-defines 'cljs.core/*target*] "nodejs")
+
+      (cond->
+        (:worker-info state)
+        (shared/inject-node-repl config)
+
+        (= :dev mode)
+        (shared/inject-preloads :main config)
+        )))
+
+(defn check-main-exists! [{:keys [compiler-env node-config] :as state}]
+  (let [{:keys [main main-ns main-fn]} node-config]
+    (when-not (get-in compiler-env [:cljs.analyzer/namespaces main-ns :defs main-fn])
+      (throw (ex-info (format "The configured main \"%s\" does not exist!" main)
+               {:tag ::main-not-found
+                :main-ns main-ns
+                :main-fn main-fn
+                :main main})))
+    state))
+
+(defn flush [state mode config]
+  (case mode
+    :dev
+    (node/flush-unoptimized state)
+    :release
+    (node/flush-optimized state)))
+
+(defn process
+  [{::comp/keys [mode stage config] :as state}]
+  (case stage
+    :configure
+    (configure state mode config)
+
+    :compile-prepare
+    (node/replace-goog-global state)
+
+    :compile-finish
+    (-> state
+        (check-main-exists!)
+        (cond->
+          (shared/bootstrap-host-build? state)
+          (shared/bootstrap-host-info)))
+
+    :flush
+    (flush state mode config)
+
+    state
+    ))
