0d7d8ff76ca15e13a41b56230b7f3525e712cc33
Kiran Shila <me@kiranshila.com>
2020-08-11 22:43:16 -0400

    Pages set up

--- src/hackerhud/core.cljs
+++ src/hackerhud/core.cljs
@@ -15,3 +15,51 @@
 
 ; Init the VFD
 (vfd/setup)
+
+; Use light to update vfd brightness
+(defn update-brightness []
+  (let [light (utils/get-light)]
+    (cond
+      (> light 0.1) (vfd/set-brightness 4)
+      :finally (vfd/set-brightness 1))))
+
+; Update brightness every second
+(set-interval update-brightness 1000)
+
+; When wifi connects, update system time
+(.on wifi "connected" utils/set-new-time)
+
+; Pages
+
+(defn day-page []
+  (let [date (utils/get-date)
+        day (utils/get-day)]
+    (vfd/display (str "Today is") 0 0)
+    (vfd/display (str day " the " date (utils/date-suffix date)) 0 1)))
+
+(defn time-page []
+  (vfd/display (utils/get-formatted-time) 6 0))
+
+(defn weather-page []
+  (let [weather @utils/current-weather]
+    (vfd/display (str "Outside is "
+                      (:description weather)) 0 0)
+    (vfd/display (str "Feels like "
+                      (:feels-like weather) "F") 0 1)))
+
+; Update weather every 15 min
+(set-interval utils/set-new-weather 900000)
+
+(def pages [day-page time-page weather-page])
+(def current-page (atom 0))
+
+(defn update-page []
+  (let [page @current-page]
+    (vfd/command :clear)
+    ((get pages page))
+    (if (= (count pages) (inc page))
+      (reset! current-page 0)
+      (swap! current-page inc))))
+
+; Update the page every 30 sec
+(set-interval update-page 30000)
--- src/hackerhud/utils.cljs
+++ src/hackerhud/utils.cljs
@@ -1,11 +1,27 @@
 (ns hackerhud.utils
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [goog.object]
+            [goog]))
 
 ;; Constants
 
 (def tz-str "America/New_York")
 (def tz -5)
 (def http (js/require "http"))
+(def light-sensor-pin js/D35)
+
+;; JS === dumb
+
+(defn obj->clj
+  [obj]
+  (if (goog.isObject obj)
+    (-> (fn [result key]
+          (let [v (goog.object/get obj key)]
+            (if (= "function" (goog/typeOf v))
+              result
+              (assoc result key (obj->clj v)))))
+        (reduce {} (.getKeys goog/object obj)))
+    obj))
 
 ;; Espruino Wrappers
 
@@ -31,6 +47,17 @@
 
 ;; Clock Utilities
 
+(defn date-suffix [day]
+  (case (mod (quot day 10) 10)
+    1 "th"
+    (case (mod day 10)
+      1 "st"
+      2 "nd"
+      3 "rd"
+      "th")))
+
+(def days ["Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday"])
+
 (defn clock-api [tz]
   (str "http://worldtimeapi.org/api/timezone/" tz))
 
@@ -49,6 +76,12 @@
          (case (quot hours24 12)
            0 " AM"
            1 " PM"))))
+
+(defn get-day []
+  (get days (.getDay (js/Date.))))
+
+(defn get-date []
+  (.getDate (js/Date.)))
 
 ;; HTTP Utilities
 
@@ -79,3 +112,39 @@
   "Perform a GET to a clock api to update system time"
   []
   (get-uri (clock-api tz-str) update-time))
+
+;; Sensors
+
+(defn get-light []
+  (if-let [value (js/analogRead light-sensor-pin)]
+    value
+    0))
+
+;; Weather
+
+(def city "Tampa")
+(def weather-api-key "1837f694725e881ebe09ab2a50f0f509")
+(def current-weather (atom nil))
+
+(defn weather-api [query api-key]
+  (str "http://api.openweathermap.org/data/2.5/weather?q="
+       query
+       "&appid="
+       api-key))
+
+(defn K-to-F [K]
+  (- (* K (/ 9 5)) 459.67))
+
+(defn update-weather []
+  (let [resp @!response
+        weather (get (.-weather resp) 0)
+        main (.-main resp)
+        now (.now js/Date)]
+    (reset! current-weather
+            {:description (.-main weather)
+             :feels-like (.round js/Math (K-to-F (.-feels_like main)))})))
+
+(defn set-new-weather
+  "Perform a GET to the weather API to update current weather"
+  []
+  (get-uri (weather-api city weather-api-key) update-weather))
